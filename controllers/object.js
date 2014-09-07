8c565defe33f4ad0e7dbad8d5fbc928608446f04
syntaxfish <syntaxfish@gmail.com>
2014-09-07 19:12:32 +0900

    client 응답 패킷 변경

--- controllers/object.js
+++ controllers/object.js
@@ -15,23 +15,24 @@ exports.create = function (req, res) {
 	// Body
 	input.class = req.params.classname;
 	input.method = 'create';
+
 	// Object
 	input.object = req.body;
-	input.object.objectId = uuid();
+	input.object.objectId = input.objectId = uuid();
 	input.object.createAt = new Date();
 	input.object.updateAt = new Date();
 
 
-	objectHandler.createObject( input, function(error, result) {
-		if(error) {
-			// send errorCode
-			return sendError(res, errorCode.OTHER_CAUSE);
-		} else {
-			// send result
-			res.json(input);
-			schemaHandler.createSchema(input);
-		}
-	});
+	// Output
+	output.objectId = input.object.objectId;
+	output.createAt = input.object.createAt;
+	output.updateAt = input.object.updateAt;
+
+
+	schemaHandler.createSchema(input);
+	objectHandler.createObject(input);
+
+	res.json(input);
 };
 
 // /classes/<className>/<objectId>		PUT	Updating Objects
@@ -49,13 +50,17 @@ exports.update = function (req, res) {
 	input.object.objectId = req.params.objectid;
 	input.object.updateAt = new Date();
 
+	// Output
+	output.objectId = input.object.objectId;
+	output.updateAt = input.object.updateAt;
+
 	objectHandler.updateObject( input, function(error, result) {
 		if(error) {
 			// send errorCode
 			return sendError(res, errorCode.OTHER_CAUSE);
 		} else {
 			// send result
-			res.json(input);
+			res.json(output);
 			schemaHandler.updateSchema(input);
 		}
 	});
@@ -71,9 +76,10 @@ exports._delete = function (req, res) {
 	// Body
 	input.class = req.params.classname;
 	input.method = 'delete';
+	input.object.objectId = req.params.objectid;
 
-	input.object = { objectId: req.params.objectid };
-
+	// Output
+	output.objectId = input.object.objectId;
 
 	objectHandler.deleteObject( input, function(error, result) {
 		if(error) {
@@ -81,7 +87,7 @@ exports._delete = function (req, res) {
 			return sendError(res, errorCode.OTHER_CAUSE);
 		} else {
 			// send result
-			res.json(input);
+			res.json(output);
 		}
 	});
 	
